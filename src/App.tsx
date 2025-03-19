4afe8b72df583d84b4eea6c678aa23ec5b4d03a8
ui.alpha <ui.alpharive@gmail.com>
2025-03-19 15:38:14 +0530

    Build Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Wallet from "./Pages/Wallet";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 export default function App() {
   return (
     <Router>
-      <div className="p-5">
+      {/* <div className="p-5"> */}
         <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registart" element={<Register />} />
           <Route path="/wallet" element={<Wallet />} />
         </Routes>
-      </div>
+      {/* </div> */}
     </Router>
   );
 }
