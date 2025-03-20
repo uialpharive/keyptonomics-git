4e21a85f140c485c66dc91e66e58cacf01324b20
Bharath Kumar <bharath.alpharive@gmail.com>
2025-03-20 17:54:20 +0530

    app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
 
 
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Wallet from "./Pages/Wallet";
+import { Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-import Home from "./Pages/Home";
-import Header from "./components/Header";
 import Stake from "./Pages/Stake";
+import Wallet from "./Pages/Wallet";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Deposit from "./Pages/Deposit";
+import Withdraw from "./Pages/Withdraw";
+const Layout = () => {
+  const location = useLocation();
+  const hideHeaderFooter = ["/login", "/registart"].includes(location.pathname);
 
+  return (
+    <div>
+      {!hideHeaderFooter && <Header />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registart" element={<Register />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/stake" element={<Stake/>} />
+        <Route path="/deposit" element={<Deposit/>} />
+        <Route path="/withdraw" element={<Withdraw/>} />
+      </Routes>
+      {!hideHeaderFooter && <Footer />}
+    </div>
+  );
+};
 export default function App() {
   
   return (
     <Router>
-      <div className="">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/wallet" element={<Wallet />} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/stake" element={<Stake/>} />
-        </Routes>
-      </div>
-    </Router>
+    <Layout />
+  </Router>
   );
 }
