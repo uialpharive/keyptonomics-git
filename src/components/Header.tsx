c4fa5785325924b62caf1d270938ba0191388af6
joshua.alpharive <sathyabamaalpharive@gmail.com>
2025-03-24 18:43:54 +0530

    header,searchpopup,use slice

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,25 @@
-import { Flag, LogoDark, LogoLight } from "../SVGComponents";
+import { History, Home, Menu, Settings, User, User2Icon, X } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { GrTransaction } from "react-icons/gr";
 import { IoIosNotifications } from "react-icons/io";
-import IconCard from "../UIComponents/IconCard";
-import { Search, User } from "lucide-react";
-import ThemeToggle from "./ThemeToggle";
-import BalanceSlider from "./BalanceSlider";
-import HeaderBtn from "../UIComponents/HeaderBtn";
+import { MdEventSeat } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { openSearch } from "../redux/slices/searchSlice";
+import { Flag, LogoDark, LogoLight } from "../SVGComponents";
+import HeaderBtn from "../UIComponents/HeaderBtn";
+import IconCard from "../UIComponents/IconCard";
+import BalanceSlider from "./BalanceSlider";
+import ThemeToggle from "./ThemeToggle";
 // import ResponsiveColorBoxes from "./ResponsiveColorBoxes";
 // import CountryFlag from "./CountryFlag"
 
 const menus = [
-    { name: "Home", path: "/" },
-    { name: "Trade", path: "/trade" },
-    { name: "Stake", path: "/stake" },
-    { name: "Transactions", path: "/transactions" },
-    { name: "Hedge FlaskRound", path: "/hedge-flaskround" },
-    { name: "Staking History", path: "/staking-history" },
-    { name: "Live Events", path: "/live-events" },
+    { name: "Home", path: "/", icon: Home },
+    { name: "Trade", path: "/trade", icon: Home },
+    { name: "Stake", path: "/stake", icon: Home },
+    { name: "Transactions", path: "/transactions", icon: GrTransaction },
+    { name: "Hedge FlaskRound", path: "/hedge-flaskround", icon: Home },
+    { name: "Staking History", path: "/staking-history", icon: History },
+    { name: "Live Events", path: "/live-events", icon: MdEventSeat },
 ];
 
 
@@ -28,13 +29,38 @@ const availableBalance = [
     { id: 3, text: "Available Balance", value: "$ 1,339.79" },
 ];
 const Header = () => {
-    const dispatch = useDispatch()
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const drawerRef = useRef<any>(null);
+
+    // Toggle Drawer
+    const toggleDrawer = () => {
+        setIsDrawerOpen((prev) => !prev);
+    };
+
+    // Close drawer when clicking outside
+    useEffect(() => {
+        // const handleClickOutside = (event: any) => {
+        //     if (drawerRef.current && !drawerRef.current.contains(event.target)) {
+        //         setIsDrawerOpen(false);
+        //     }
+        // };
+        const handleClickOutside = (event: MouseEvent) => {
+            if (drawerRef.current && !drawerRef.current.contains(event.target as Node)) {
+                setIsDrawerOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+    // const dispatch = useDispatch()
     const iconData = [
-        { id: 1, Icon: Search, onClick: () => dispatch(openSearch()) },
-        { id: 2, Icon: IoIosNotifications },
-        { id: 3, Icon: Flag },
-        { id: 4, Icon: ThemeToggle },
-        { id: 5, Icon: User },
+        // { id: 1, Icon: Search, onClick: () => dispatch(openSearch()) },
+        { id: 1, Icon: IoIosNotifications },
+        { id: 2, Icon: Flag },
+        { id: 3, Icon: ThemeToggle },
+        { id: 4, Icon: User },
     ];
     return (
         <>
@@ -104,11 +130,13 @@ const Header = () => {
                         />
                         <div className="flex gap-4 lg:hidden">
                             <IconCard Icon={User} />
+                            <IconCard Icon={Menu} onClick={toggleDrawer} />
+
                         </div>
                     </div>
                     <div className="hidden lg:flex gap-4">
-                        {iconData.map(({ id, Icon, onClick }) => (
-                            <IconCard key={id} Icon={Icon} onClick={onClick} />
+                        {iconData.map(({ id, Icon, }) => (
+                            <IconCard key={id} Icon={Icon} />
                         ))}
                     </div>
 
@@ -136,9 +164,74 @@ const Header = () => {
                         ))}
                     </div>
                 </nav>
+                {/* Side Drawer */}
+                <div
+                    className={`fixed top-0 right-0 h-full w-64 bg-white dark:bg-gray-800 shadow-lg transform ${isDrawerOpen ? "translate-x-0" : "translate-x-full"
+                        } transition-transform duration-300 ease-in-out z-50`}
+                    ref={drawerRef}
+                >
+                     <hr className="h-[3px] bg-themeBlue  border-0" />
+                    {/* Close Button */}
+                    <div className="flex justify-between items-center px-5 py-4 border-b dark:border-gray-700">
+                        <h2 className="text-lg font-bold dark:text-white">User Menu</h2>
+                        <IconCard Icon={ThemeToggle}/>
+                        <button onClick={toggleDrawer}>
+                            <X className="w-6 h-6 text-gray-600 dark:text-white" />
+                        </button>
+                    </div>
+
+                    {/* Menu Items */}
+                    <div className="flex flex-col gap-5 px-1">
+                        <div className="flex flex-col gap-1 p-4">
+                            {availableBalance.map((item, index) => (
+                                <div className="flex gap-4 items-center dark:bg-inputBg shadow-xl rounded-lg p-4" key={index}>
+                                    <div className="flex flex-col justify-end">
+                                        <h1 className="text-[12px] font-lato font-medium dark:text-white">
+                                            {item.text}
+                                        </h1>
+                                    </div>
+
+                                    <p className="text-[11px] font-extrabold text-themeBlue flex items-center">
+                                        {item.value}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            {menus.map((item) => (
+                                <Link
+                                    key={item.name}
+                                    to={item.path}
+                                    className="flex items-center gap-3 font-lato font-bold text-gray-700 py-2 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 px-3 rounded cursor-pointer"
+                                >
+                                    <item.icon className="w-5 h-5 text-gray-500 dark:text-gray-300" />
+                                    {item.name}
+                                </Link>
+                            ))}
+                            <div className="flex items-center gap-3 px-3">
+                                <User2Icon className="w-5 h-5 text-gray-500 dark:text-gray-300" />
+
+                                <Link to="/profile" className="py-2 text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 rounded">Profile</Link>
+                            </div>
+                            <div className="flex items-center gap-3 px-3">
+                                <Settings className="w-5 h-5 text-gray-500 dark:text-gray-300" />
+
+                                <Link to="/settings" className="py-2 text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 rounded">Settings</Link>
+                            </div>
+                        </div>
+
+                        <button className="py-2 text-red-600 dark:text-red-400 hover:bg-gray-100 dark:hover:bg-gray-700 px-3 rounded">Logout</button>
+                    </div>
+                </div>
                 <BalanceSlider availableBalance={availableBalance} />
                 {/* <ResponsiveColorBoxes /> */}
-
+                {/* Background overlay when menu is open */}
+                {isDrawerOpen && (
+                    <div
+                        className="fixed inset-0 bg-black bg-opacity-50 z-40"
+                        onClick={toggleDrawer}
+                    ></div>
+                )}
             </header>
         </>
     );
